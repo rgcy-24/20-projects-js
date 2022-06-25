561c507e0daf9603a8e2607f4a18d4f59e327e5b
abyo <alexis.ollandfr@gmail.com>
2022-06-25 13:32:38 +0200

    Fix projet #2

--- 02 - Bulls and Cows/main.js
+++ 02 - Bulls and Cows/main.js
@@ -1,18 +1,19 @@
 let attempts = 0; let bulls = 0; let cows = 0;
 let secretNumber = generateSecretNumber();
+console.log(secretNumber);
 
 let roundStats = {
   round: 1,
   wins: 0,
   loses: 0
-};
+}
 
 function checkGuess() {
   let guess = document.getElementById("guessInput").value;
   let secretString = secretNumber.join('');
   bulls = 0; cows = 0;
 
-  let checkGuessLength = new Set(guess);
+  const checkGuessLength = new Set(guess);
 
   if (guess.length !== checkGuessLength.size || guess.length !== 4) {
     document.getElementById("logsArea").value += `${guess} est invalide, veuillez entrer un nombre composé de exactement 4 chiffres différents.\n`;
@@ -39,8 +40,7 @@ function checkGuess() {
     return playAgain();
   }
 
-  document.getElementById("logsArea").value += `${guess} - ${bulls}B ${cows}C, try: ${attempts}.\n`;
-  printGameStats();
+  document.getElementById("logsArea").value += `${guess} - ${bulls}B ${cows}C, try: ${attempts}\n`;
 }
 
 function playAgain() {
@@ -51,19 +51,19 @@ function playAgain() {
 }
 
 function printGameStats() {
-  let gameStats = document.getElementById("gameStats");
+  const gameStats = document.getElementById("gameStats");
   gameStats.innerHTML = `R: ${roundStats.round} | V: ${roundStats.wins} | D: ${roundStats.loses}`;
 }
 
 function generateSecretNumber() {
-  const numbers = Array.from({ length: 9 }, (v, k) => k + 1);
+  const numbers = Array.from({ length: 9}, (v, k) => k + 1);
   let currentIndex = numbers.length, randomIndex;
 
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    [numbers[currentIndex], numbers[randomIndex]] = [numbers[randomIndex], numbers[currentIndex]]
+    [numbers[currentIndex], numbers[randomIndex]] = [numbers[randomIndex], numbers[currentIndex]];
   }
 
   return numbers.slice(0, 4);
@@ -74,5 +74,5 @@ function clearLogs() {
 }
 
 function printRules() {
-  alert("Entrez un nombre composé de 4 chiffres différents dans la case à côté de 'Guess'. L'ordinateur le compare avec le code secret et vous donne deux indices: les numéros des 'bulls' (B) et des 'cows' (C). Qu'est ce que cela signifie? Un 'bulls' est un chiffre qui est présent dans les deux codes à la même position. Un 'cows' est un chiffre qui est présent dans les deux codes mais à une position différente. Par exemple, si le code secret est 7512 et que vous essayez 5392, la réponse sera '1B 1C' (1 bull 1 cow). C'est tout!");
+  alert("Entrez un nombre composé de 4 chiffres différents dans la case à côté de 'Guess'. L'ordinateur le compare avec le code secret et vous donne deux indices: les numéros 'bulls' (B) et des cows (C). Qu'est ce que cela signifie? Un 'bulls' est un chiffre qui est présent dans les deux codes à la même position. Un 'cows' est un chiffre qui est présent dans les deux codes mais à une position différente. Par exemple, si le code secret est 7512 et que vous essayez 5392, la réponse sera '1B 1C' (1 bull 1 cow). C'est tout!")
 }
